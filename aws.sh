fcf3f3f6308b62b4c8e8197445d46a6a83df56ed
Sakol Assawasagool <koobitor@gmail.com>
2015-12-12 16:07:18 +0700

    add aws.sh

---
+++ aws.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~
+
+#--------------------------------------------------
+# Install aws
+#--------------------------------------------------
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+export PATH=$PATH:/usr/local/bin
+rm -Rf awscli-bundle
+rm -Rf awscli-bundle.zip
+aws --version
+
+#--------------------------------------------------
+# Configure aws
+#--------------------------------------------------
+echo "\n---- Configure aws ----"
+echo "\n AWS Access Key -> https://console.aws.amazon.com/iam"
+aws configure
